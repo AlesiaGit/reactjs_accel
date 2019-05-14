b1b88fcee7523c1f329dcbc95fd4befb214126a7
AlesiaGit <stolepchenko@gmail.com>
2019-05-14 16:10:22 +0300

    Updates

---
+++ precache-manifest.dea5de84bb12d5249de84b5f930c71e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5743b7606a243c9bd45815acef8191c0",
+    "url": "/reactjs_accel/index.html"
+  },
+  {
+    "revision": "17dfa5b34d4ea92b22dd",
+    "url": "/reactjs_accel/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/reactjs_accel/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "17dfa5b34d4ea92b22dd",
+    "url": "/reactjs_accel/static/js/main.aa687c22.chunk.js"
+  },
+  {
+    "revision": "1e61c4af176a06cbe2f2",
+    "url": "/reactjs_accel/static/js/runtime~main.dd179d6e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactjs_accel/static/media/logo.5d5d9eef.svg"
+  }
+]);
