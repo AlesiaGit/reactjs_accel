e213eca53430d747379182dcccbf7b44cd5e8c35
AlesiaGit <stolepchenko@gmail.com>
2019-05-16 19:18:08 +0300

    Updates

--- precache-manifest.0059dda58e7d09677683bc1523d1ae1f.js
+++ precache-manifest.0059dda58e7d09677683bc1523d1ae1f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "619309727460d0c555d50af4fbcbacd5",
+    "revision": "29bacfd463df8d6fe26470e3d909ebc2",
     "url": "/reactjs_accel/index.html"
   },
   {
-    "revision": "07e436cbc0189d64b37b",
+    "revision": "f3874ec264bf61bc3001",
     "url": "/reactjs_accel/static/css/main.2cce8147.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reactjs_accel/static/js/2.a5adb38c.chunk.js"
   },
   {
-    "revision": "07e436cbc0189d64b37b",
-    "url": "/reactjs_accel/static/js/main.5a8740dc.chunk.js"
+    "revision": "f3874ec264bf61bc3001",
+    "url": "/reactjs_accel/static/js/main.6f9d1a54.chunk.js"
   },
   {
     "revision": "1e61c4af176a06cbe2f2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactjs_accel/precache-manifest.75b880e25e9f37e455d3c0bd17bdcade.js"
+  "/reactjs_accel/precache-manifest.0059dda58e7d09677683bc1523d1ae1f.js"
 );
 
 self.addEventListener('message', (event) => {
